bf34d66d8a7f9017d033497847566be3cc46e8ca
Amo <ifeelgooder@gmail.com>
2019-06-17 19:27:54 +0800

    Update main.c

--- HallSensor/main.c
+++ HallSensor/main.c
@@ -127,14 +127,14 @@ void servo(int servo, int angle) {
 	int value = 0; //count of units
 	switch (servo) {
 		case 0:
-			period_per_unit = (1/PWM_BASE_FREQ)*PWM_CHANNEL_0_CLOCK*1000; //ms
+			period_per_unit = (1.0f/PWM_BASE_FREQ)*PWM_CHANNEL_0_CLOCK*1000.0f; //ms
 			duty = (period_per_unit*SERV_0_DUTY_90)-((90-angle)/180)*10*period_per_unit; //1.5ms for 0, 2ms for 90, 1ms for -90
 			value = duty/period_per_unit;
 			LOG("%s servo_0 going to pwmWrite:%d, duty:%f, unit:%f\n", LIGHT_GRAY, value, duty, period_per_unit);
 			pwmWrite(SERVO_0, value);
 			break;
 		case 1:
-			period_per_unit = (1/PWM_BASE_FREQ)*PWM_CHANNEL_0_CLOCK*1000;
+			period_per_unit = (1.0f/PWM_BASE_FREQ)*PWM_CHANNEL_0_CLOCK*1000.0f;
 			duty = (period_per_unit*SERV_0_DUTY_90)-((90-angle)/180)*10*period_per_unit; //1.5ms for 0, 2ms for 90, 1ms for -90
 			value = duty/period_per_unit;
 			LOG("%s servo_1 going to pwmWrite:%d, duty:%f, unit:%f\n", LIGHT_GRAY, value, duty, period_per_unit);
